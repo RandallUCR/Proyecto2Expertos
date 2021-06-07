0c48c256222be945b56cf3f6017a10b58288755d
Randall <randallgears1998@gmail.com>
2021-06-07 17:16:24 -0600

    Web API

---
+++ Web_API_Proyecto2/Web_API_Proyecto2/Models/DestinoUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Proyecto2.Models
+{
+    public class DestinoUtil
+    {
+        public int TN_Id { set; get; }
+        public string TC_Nombre { set; get; }
+        public string TC_Tipo_Precio { set; get; }
+        public string TC_Tipo_Visitas { set; get; }
+        public string TC_Tipo_Turismo { set; get; }
+        public string TC_Ubicacion { set; get; }
+        public decimal TN_Precio { set; get; }
+        public string TC_Ruta_Imagen { set; get; }
+
+    }
+}
